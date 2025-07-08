e17bea2b2a30ae8c0a3f67c949a878686a9d2587
Maic369 <maicolgarza5@gmail.com>
2025-07-08 16:39:57 -0500

    commit

---
+++ debitos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Debito(models.Model):
+  banco = models.TextField("Banco")
+  valor = models.IntegerField("Valor")
+  Banco_id = models.TextField("Banco_id")
